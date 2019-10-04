3e139b12d8be53dfc1d49a053aadb7c78487c14f
Caams CM <ccmiranda@ciencias.unam.mx>
2019-10-03 19:35:13 -0500

    Documente algunas clases.

--- src/FactoryProducer.java
+++ src/FactoryProducer.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 public class FactoryProducer{
 
-  public Maquina getFactory(String tipo){
+  public Maquina getFactory(String tipo, ArrayList<Ingrediente> ingredientes){
     if(tipo.equalsIgnoreCase("dulce"))
-      return new Wonka3000();
+      return new Wonka3000(ingredientes);
     else if(tipo.equalsIgnoreCase("galleta"))
-      return new Galleneitor5000();
+      return new Galleneitor5000(ingredientes);
     return null;
   }
 
