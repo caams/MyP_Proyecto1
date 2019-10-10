f6a57c00de71bc3ad0a5423c58bb2107dec1e01b
Caams CM <ccmiranda@ciencias.unam.mx>
2019-10-09 22:45:53 -0500

    Termine de documentar lo necesario

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * Clase abstracta Chocolate que extiende de Producto.
+ */
 public abstract class Chocolate extends Producto{
 
+  /**
+   * Contructor de la clase abstracta Chocolate.
+   * Se crea una instancia de la máquina encargada de los chocolates y se 
+   * añaden los ingredientes para los chocolates a la lista de ingredientes.
+   */
   public Chocolate(){
     this.creador = Wonka3000.getInstance();
     this.setIngredientes(new ArrayList<Ingrediente>());
@@ -10,10 +18,12 @@ public abstract class Chocolate extends Producto{
     this.getIngredientes().add(new Ingrediente("Azúcar", 8, 6400));
   }
 
+  /* Se elige el molde de cada chocolate. */
   @Override public void elegirMolde(){
     System.out.println("Tomando molde para chocolates...");
   }
 
+  /* Se enmolda la mezcla de chocolates. */
   @Override public void enmoldar(){
     System.out.println("Vaciando mezcla en el molde para chocolates...");
   }
--- src/Galleta.java
+++ src/Galleta.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 
 /**
- * Clase abstracta Galleta
+ * Clase abstracta Galleta. Las galletas tienen un nombre, sus propios ingredientes y una máquina productora.
  */
 public abstract class Galleta{
 
+  /* Nombre de la galleta. */
   protected String nombre;
+  /* Lista de los ingredientes para las galletas. */
   protected ArrayList<Ingrediente> ingredientes;
+  /* Máquina productora de las galletas. */
   protected Maquina productora;
 
+  /**
+   * Contructor de la clase abstracta Galleta.
+   * Se crea una instancia de la máquina encargada de las galletas y se 
+   * añaden los ingredientes para las galletas a la lista de ingredientes.
+   */
   public Galleta(){
     this.productora = Galleneitor5000.getInstance();
     this.ingredientes = new ArrayList<Ingrediente>();
@@ -18,14 +26,27 @@ public abstract class Galleta{
     this.ingredientes.add(new Ingrediente("Huevo", 12, 3000));
   }
 
+  /**
+   * Regresa el nombre de la galleta.
+   * @return el nombre de la galleta.
+   */
   public String getNombre(){
     return this.nombre;
   }
 
+  /**
+   * Regresa la lista de los ingredientes de las galletas.
+   * @return la lista de los ingredientes de las galletas.
+   */
   public ArrayList<Ingrediente> getIngredientes(){
     return this.ingredientes;
   }
 
+  /**
+   * Si la máquina productora tiene un estado aceptable agrega un ingrediente a la vez.
+   * Después de usar dicho ingrediente, se decrementa la cantidad del ingrediente usado.
+   * @param ingrediente el ingrediente para preparar la galleta.
+   */
   public void utilizarIngrediente(String ingrediente){
     if(this.productora.getEstadoActual() instanceof EstadoPreparando){
       Ingrediente ing = this.getIngredient(ingrediente);
@@ -40,6 +61,11 @@ public abstract class Galleta{
     }
   }
 
+  /**
+   * Método que regresa un ingrediente del tipo que se pide.
+   * @param ingrediente que se pide regresar.
+   * @return el ingrediente que se solicita si existe, null en otro caso.
+   */
   public Ingrediente getIngredient(String ingrediente){
     for(Ingrediente i : this.getIngredientes()){
       if(i.getNombre().equals(ingrediente))
@@ -48,6 +74,9 @@ public abstract class Galleta{
     return null;
   }
 
+  /**
+   * Método template para preparar la receta de cada galleta.
+   */
   public void prepararGalleta(){
     elegirMolde();
     prepararMasa();
@@ -56,19 +85,24 @@ public abstract class Galleta{
     agregarComplemento();
   }
 
+  /* Elige el molde default de cada galleta. */
   public void elegirMolde(){
     System.out.println("Tomando molde para galletas...");
   }
 
+  /* Enmolda la masa de las galletas. */
   public void enmoldar(){
     System.out.println("Colocando la masa en los moldes...");
   }
 
+  /* Hornea las galletas. */
   public void hornear(){
     System.out.println("Horneando las galletas...");
   }
 
+  /* Método abstracto para preparar la masa de cada galleta. */
   public abstract void prepararMasa();
+  /* Método abstracto para agregar un complemento a cada galleta. */
   public abstract void agregarComplemento();
 
 }
